003b51e1a7248eea4401d8605dcb29a51cacbeb7
kailashUndamatla <50083757+kailashUndamatla@users.noreply.github.com>
2019-04-29 01:41:34 +0530

    Update player.js
    Added error handling code

--- player.js
+++ player.js
@@ -2,3 +2,6 @@
 $.getJSON("http://localhost:8080/Oraclereq/guide.JSON", function(data){
 	alert(data);
 })
+.success(function() { alert("second success"); })
+.error(function(jqXHR, textStatus, errorThrown) { alert(errorThrown); })
+.complete(function() { alert("complete"); });
