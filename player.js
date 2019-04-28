99e4f1a105967204b15bc4540934671a3c9cd19d
kailashUndamatla <50083757+kailashUndamatla@users.noreply.github.com>
2019-04-29 02:00:46 +0530

    Update player.js
    Adding title attr with respective JSON data value

--- player.js
+++ player.js
@@ -1,7 +1,7 @@
 //ADD YOUR CODE HERE.
 $.getJSON("http://localhost:8080/Oraclereq/guide.JSON", {'mimeType' : 'application/json'}, function(data){
 	data.steps.forEach(function(subData){
-		alert(subData.selector);
+		$(subData.selector).attr('title', subData.content);
 	});
 })
 .success(function() { alert("second success"); })
