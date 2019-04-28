fe6ee1844a0e719ea11a96f5b53fb344ef740ce8
kailashUndamatla <50083757+kailashUndamatla@users.noreply.github.com>
2019-04-29 01:57:46 +0530

    Update player.js
    After remove of JSON object content. Reading #id_1

--- player.js
+++ player.js
@@ -1,6 +1,8 @@
 //ADD YOUR CODE HERE.
 $.getJSON("http://localhost:8080/Oraclereq/guide.JSON", {'mimeType' : 'application/json'}, function(data){
-	alert(data);
+	data.steps.forEach(function(subData){
+		alert(subData.selector);
+	});
 })
 .success(function() { alert("second success"); })
 .error(function(jqXHR, textStatus, errorThrown) { alert(errorThrown); })
